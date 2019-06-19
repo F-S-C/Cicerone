813b03a3668e3d1e095de10c7a0cea1f6285fa11
Ax3lFernus <a.annese99@gmail.com>
2019-06-19 15:07:45 +0200

    Adattati i connettori al db finale

--- Server/db_connector/insert_reservation.php
+++ Server/db_connector/insert_reservation.php
@@ -7,12 +7,12 @@
 */
 include('configuration.php');
 
-$query = "INSERT INTO reservation (username, booked_itinerary, number_of_children, number_of_adults, total, requested_date, forwarding_date, confirmation_date) VALUES (";
+$query = "INSERT INTO reservation (username, booked_itinerary, number_of_children, number_of_adults, total, requested_date, forwarding_date, confirm_date) VALUES (";
 
 // Checks if the variables passed via POST exist and adds them to the query
-if(isset($_POST['username']) && isset($_POST['booked_itinerary']) && isset($_POST['number_of_children']) && isset($_POST['number_of_adults']) && isset($_POST['total']) && isset($_POST['requested_date']) && isset($_POST['forwarding_date']) && isset($_POST['confirmation_date']))
+if(isset($_POST['username']) && isset($_POST['booked_itinerary']) && isset($_POST['number_of_children']) && isset($_POST['number_of_adults']) && isset($_POST['total']) && isset($_POST['requested_date']) && isset($_POST['forwarding_date']) && isset($_POST['confirm_date']))
 	{
-		$query .= "'" . strtolower($_POST['username']) . "','" . $_POST['booked_itinerary'] . "','" . $_POST['number_of_children'] . "','" . $_POST['number_of_adults'] . "','" . $_POST['total'] . "','" . $_POST['requested_date'] . "','" . $_POST['forwarding_date'] . "','" . $_POST['confirmation_date'] . "')";
+		$query .= "'" . strtolower($_POST['username']) . "','" . $_POST['booked_itinerary'] . "','" . $_POST['number_of_children'] . "','" . $_POST['number_of_adults'] . "','" . $_POST['total'] . "','" . $_POST['requested_date'] . "','" . $_POST['forwarding_date'] . "','" . $_POST['confirm_date'] . "')";
 		
 		// Send the query to MySQL and print result in JSON
 		$sth = mysqli_query($conn, $query);
