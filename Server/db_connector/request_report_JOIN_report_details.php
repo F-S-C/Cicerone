9f128dae26714a5f793284a675966c8541140b04
Ax3lFernus <a.annese99@gmail.com>
2019-06-21 22:44:30 +0200

    Corretto bug e aggiunto un connettore
    - Corretto un bug che non permetteva l'inserimento di condizioni nella query in alcuni script
    - Aggiunta lo script che restituisce un'array in json degli elementi della tabella: report JOIN report_details

---
+++ Server/db_connector/request_report_JOIN_report_details.php
@@ -0,0 +1,31 @@
+<?php
+/*
+	Project: Cicerone
+	Team: FSC
+	File description: PHP connector that enables communication between Android 
+	and MySQL. This file request data from table 'report'.
+*/
+include('configuration.php');
+
+$query = "SELECT * FROM report INNER JOIN report_details ON report.report_code = report_details.report_code";
+$condition = Array();
+
+// Checks if the variables passed via POST exist and adds them to the query
+if(isset($_POST['username']))
+{
+	array_push($condition, "username = '" . strtolower($_POST['username']) . "'");
+}
+if(isset($_POST['reported_user']))
+{
+	array_push($condition, "reported_user = '" . strtolower($_POST['reported_user']) . "'");
+}
+if(isset($_POST['report_code']))
+{
+	array_push($condition, "report_code = '" . $_POST['report_code'] . "'");
+}
+
+addConditionsToQuery($condition, $query);
+
+// Send the query to MySQL and print received records in JSON
+requestData($conn, $query);
+?>
