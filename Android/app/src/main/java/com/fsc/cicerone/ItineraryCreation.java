aa52e4d1a3c63582abc9c85d4da6e317699af5c3
grazianomontanaro <40232320+prewarning@users.noreply.github.com>
2019-06-25 18:02:31 +0200

    IF - 9 (3)
    Inseriti icon Calendario per scegliere data inizio e fine itinerario

--- Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
+++ Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
@@ -2,14 +2,18 @@ package com.fsc.cicerone;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.InputType;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -20,31 +24,81 @@ import java.util.TimeZone;
 
 public class ItineraryCreation<AddDetails> extends AppCompatActivity {
     EditText beginningDate;
-    DatePickerDialog datePicker;
+    EditText endingDate;
+    ImageButton selectBeginningDate;
+    ImageButton selectEndingDate;
+    final Calendar myCalendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_creation);
+         beginningDate = findViewById(R.id.inputBeginningDate);
+         endingDate = findViewById(R.id.inputEndingDate);
+         beginningDate.setInputType(InputType.TYPE_NULL);
+         selectBeginningDate = findViewById(R.id.imageButton);
+         selectEndingDate = findViewById(R.id.imageButton2);
+        DatePickerDialog.OnDateSetListener bDate = new DatePickerDialog.OnDateSetListener() {
 
-        beginningDate = findViewById(R.id.inputBeginningDate);
-        beginningDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updatebeginningDate();
+            }
+
+        };
+
+        selectBeginningDate.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                final Calendar cal = Calendar.getInstance();
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-                int month = cal.get(Calendar.MONTH);
-                int year = cal.get(Calendar.YEAR);
-                //DATE PICKER DIALOG
-                datePicker = new DatePickerDialog(ItineraryCreation.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        beginningDate.setText (dayOfMonth +"-" + month + "-" + year);
-                    }
-                }, year, month, day);
-                datePicker.show();
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ItineraryCreation.this, bDate, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+        DatePickerDialog.OnDateSetListener eDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateEndingDate();
+            }
+        };
+
+        selectEndingDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(ItineraryCreation.this, eDate, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        }
+
+    private void updatebeginningDate() {
+        String myFormat = "dd-MM-yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        beginningDate.setText(sdf.format(myCalendar.getTime()));
     }
+    private void updateEndingDate() {
+        String myFormat = "dd-MM-yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        endingDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
 
 
