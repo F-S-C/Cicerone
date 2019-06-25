d6b5dee23d31d4ad34d0b4981181abf795a1c103
espositoandrea <esposito_andrea99@hotmail.com>
2019-06-25 20:28:00 +0200

    Varie modifiche

--- Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
+++ Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
@@ -1,49 +1,34 @@
 package com.fsc.cicerone;
 
-import android.app.Activity;
 import android.app.DatePickerDialog;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.Contacts.Data;
-import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.text.InputType;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
-import java.util.TimeZone;
 
-import static android.app.DatePickerDialog.*;
+import static android.app.DatePickerDialog.OnDateSetListener;
 
 
-public class ItineraryCreation<AddDetails> extends AppCompatActivity {
-    EditText beginningDate;
-    EditText endingDate;
-    ImageButton selectBeginningDate;
-    ImageButton selectEndingDate;
+public class ItineraryCreation extends AppCompatActivity {
+    EditText selectBeginningDate;
+    EditText selectEndingDate;
     final Calendar myCalendar = Calendar.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_creation);
-         beginningDate = findViewById(R.id.inputBeginningDate);
-         endingDate = findViewById(R.id.inputEndingDate);
-         beginningDate.setInputType(InputType.TYPE_NULL);
-         selectBeginningDate = findViewById(R.id.imageButton);
-         selectEndingDate = findViewById(R.id.imageButton2);
-         selectEndingDate.setClickable(false);
-         selectEndingDate.setActivated(false);
+        selectBeginningDate = findViewById(R.id.inputBeginningDate);
+        selectEndingDate = findViewById(R.id.inputEndingDate);
+
         OnDateSetListener bDate = new OnDateSetListener() {
 
             @Override
@@ -66,8 +51,10 @@ public class ItineraryCreation<AddDetails> extends AppCompatActivity {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(ItineraryCreation.this, bDate, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH));
-                        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-                        datePickerDialog.show();
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+                selectBeginningDate.clearFocus();
+                selectEndingDate.setText(null);
             }
         });
         OnDateSetListener eDate = new OnDateSetListener() {
@@ -93,27 +80,40 @@ public class ItineraryCreation<AddDetails> extends AppCompatActivity {
                         myCalendar.get(Calendar.DAY_OF_MONTH));
                 Date minDate = null;
                 try {
-                    minDate = new SimpleDateFormat("dd-MM-yy").parse(beginningDate.getText().toString());
+                    minDate = new SimpleDateFormat("dd-MM-yy").parse(selectBeginningDate.getText().toString());
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
-                datePickerDialog.getDatePicker().setMinDate(minDate.getTime());
-                datePickerDialog.show();
+                if (minDate != null) {
+                    datePickerDialog.getDatePicker().setMinDate(minDate.getTime());
+                    datePickerDialog.show();
+                } else {
+                    Toast.makeText(ItineraryCreation.this, ItineraryCreation.this.getString(R.string.error_insert_beginning_date), Toast.LENGTH_SHORT).show();
+                }
             }
         });
-        }
+
+        selectBeginningDate.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) selectBeginningDate.callOnClick();
+        });
+
+        selectEndingDate.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) selectBeginningDate.callOnClick();
+        });
+    }
 
     private void updateBeginningDate() {
         String myFormat = "dd-MM-yy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        beginningDate.setText(sdf.format(myCalendar.getTime()));
+        selectBeginningDate.setText(sdf.format(myCalendar.getTime()));
         selectEndingDate.setClickable(true);
         selectEndingDate.setActivated(true);
     }
+
     private void updateEndingDate() {
         String myFormat = "dd-MM-yy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-        endingDate.setText(sdf.format(myCalendar.getTime()));
+        selectEndingDate.setText(sdf.format(myCalendar.getTime()));
     }
 
 }
