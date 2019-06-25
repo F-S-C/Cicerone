cfd7cb5439f96f59f44e3a55a49266ac3febbb1d
grazianomontanaro <40232320+prewarning@users.noreply.github.com>
2019-06-25 13:59:52 +0200

    IF - 9 (2)
    Tentativo di aggiunta DatePicker su beginningDate

--- Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
+++ Android/app/src/main/java/com/fsc/cicerone/ItineraryCreation.java
@@ -1,13 +1,46 @@
 package com.fsc.cicerone;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class ItineraryCreation extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
+
+public class ItineraryCreation<AddDetails> extends AppCompatActivity {
+    EditText beginningDate = findViewById(R.id.inputBeginningDate);
+    DatePickerDialog datePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_itinerary_creation);
+        beginningDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+                //DATE PICKER DIALOG
+                datePicker = new DatePickerDialog(ItineraryCreation.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        beginningDate.setText (dayOfMonth +"-" + month + "-" + year);
+                    }
+                }, year, month, day);
+                datePicker.show();
+            }
+        });
     }
 }
+
+
