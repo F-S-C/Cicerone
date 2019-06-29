4b3b04fda37fadcb2135a9590a1e112ae3f0d52a
espositoandrea <esposito_andrea99@hotmail.com>
2019-06-29 10:24:25 +0200

    Miglioramento integrazione splashscreen

--- Android/app/src/main/java/com/fsc/cicerone/LoginActivity.java
+++ Android/app/src/main/java/com/fsc/cicerone/LoginActivity.java
@@ -12,15 +12,10 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Objects;
-
-import app_connector.ConnectorConstants;
-import app_connector.DatabaseConnector;
-import app_connector.SendInPostConnector;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,16 +29,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_login);
-
-        SharedPreferences preferences = getSharedPreferences("com.fsc.cicerone", Context.MODE_PRIVATE);
-        try {
-            JSONObject currentLoggedUser = new JSONObject(preferences.getString("session", ""));
-            if (!currentLoggedUser.getString("username").equals("") && !currentLoggedUser.getString("password").equals("")) {
-                attemptLogin(currentLoggedUser, true);
-            }
-        } catch (JSONException e) {
-            Log.e(ERROR_TAG, e.toString());
-        }
 
         usernameEditText = findViewById(R.id.usernameInput);
         passwordEditText = findViewById(R.id.passwordInput);
@@ -69,7 +54,7 @@ public class LoginActivity extends AppCompatActivity {
             Log.e(ERROR_TAG, e.toString());
         }
 
-        attemptLogin(user, false);
+        attemptLogin(user);
 
     }
 
@@ -85,47 +70,39 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, "Sorry, Work In Progress", Toast.LENGTH_SHORT).show();
     }
 
-    private void attemptLogin(JSONObject user, Boolean isAutomatic) {
+    private void attemptLogin(JSONObject user) {
         RelativeLayout progressBar = findViewById(R.id.loginProgressBarContainer);
 
-        SendInPostConnector connector = new SendInPostConnector(ConnectorConstants.LOGIN_CONNECTOR, new DatabaseConnector.CallbackInterface() {
-            @Override
-            public void onStartConnection() {
-                progressBar.setVisibility(View.VISIBLE);
-                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        AccountManager accountManager = new AccountManager();
+
+        accountManager.attemptLogin(user, () -> {
+            progressBar.setVisibility(View.VISIBLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        }, jsonArray -> {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            JSONObject result = jsonArray.getJSONObject(0);
+            boolean done = false;
+            try {
+                Log.e("CIAO", String.valueOf(result.getString("result")));
+                done = result.getBoolean("result");
+            } catch (JSONException e) {
+                Log.e(ERROR_TAG, e.toString());
+            }
+            if (!done) {
+                Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_SHORT).show();
+                usernameEditText.setError(getString(R.string.wrong_credentials));
+                passwordEditText.setError(getString(R.string.wrong_credentials));
+                return;
             }
 
-            @Override
-            public void onEndConnection(JSONArray jsonArray) throws JSONException {
-                progressBar.setVisibility(View.GONE);
-                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-                JSONObject result = jsonArray.getJSONObject(0);
-                boolean done = false;
-                try {
-                    Log.e("CIAO", String.valueOf(result.getString("result")));
-                    done = result.getBoolean("result");
-                } catch (JSONException e) {
-                    Log.e(ERROR_TAG, e.toString());
-                }
-                if (!done && !isAutomatic) {
-                    Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_SHORT).show();
-                    usernameEditText.setError(getString(R.string.wrong_credentials));
-                    passwordEditText.setError(getString(R.string.wrong_credentials));
-                    return;
-                }
+            SharedPreferences preferences = getSharedPreferences("com.fsc.cicerone", Context.MODE_PRIVATE);
+            preferences.edit().putString("session", user.toString()).apply();
 
-                SharedPreferences preferences = getSharedPreferences("com.fsc.cicerone", Context.MODE_PRIVATE);
-                preferences.edit().putString("session", user.toString()).apply();
-
-                // TODO: Change activity
-//                startActivity(new Intent(LoginActivity.this, AccountDetails.class));
-                startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
-            }
+            // TODO: Change activity
+            // startActivity(new Intent(LoginActivity.this, AccountDetails.class));
+            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
         });
-
-        connector.setObjectToSend(user);
-
-        connector.execute();
     }
 }
