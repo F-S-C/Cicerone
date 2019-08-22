8d305a73a7878501c7ed93cfcd1105ec93652cf3
espositoandrea <esposito_andrea99@hotmail.com>
2019-08-22 19:07:28 +0200

    Correzione di un errore nel connettore "RequestReportDetails"
    Si è quindi rigenerata la documentazione del codice PHP per rispecchiare le modifiche apportate.

--- Server/db_connector/RequestReportDetails.php
+++ Server/db_connector/RequestReportDetails.php
@@ -12,16 +12,16 @@ require_once("JsonConnector.php");
  */
 class RequestReportDetails extends JsonConnector
 {
-    /** @var string|null The report's code. */
+    /** @var int|null The report's code. */
     private $code;
 
     /**
      * RequestReportDetails constructor.
-     * @param string|null $code The report's code.
+     * @param int|null $code The report's code.
      */
-    public function __construct(string $code = null)
+    public function __construct(int $code = null)
     {
-        $this->code = isset($code) && $code != "" ? $code : null;
+        $this->code = isset($code) ? $code : null;
         parent::__construct();
     }
 
