03f1ad01da49bf21cffcf39b88b8f86687cc23b2
espositoandrea <esposito_andrea99@hotmail.com>
2019-09-17 10:20:43 +0200

    Correzioni errori di merge

--- Android/app/src/main/java/com/fsc/cicerone/adapter/ItineraryAdapter.java
+++ Android/app/src/main/java/com/fsc/cicerone/adapter/ItineraryAdapter.java
@@ -11,11 +11,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.fsc.cicerone.manager.AccountManager;
 import com.fsc.cicerone.AdminItineraryDetails;
 import com.fsc.cicerone.ItineraryDetails;
 import com.fsc.cicerone.ItineraryManagement;
 import com.fsc.cicerone.R;
+import com.fsc.cicerone.manager.AccountManager;
 import com.fsc.cicerone.model.Itinerary;
 import com.fsc.cicerone.model.UserType;
 import com.squareup.picasso.Picasso;
@@ -61,14 +61,12 @@ public class ItineraryAdapter extends RecyclerView.Adapter<ItineraryAdapter.View
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String title = mData.get(position).getTitle();
-        //Integer itineraryNumber = mData.get(position).getCode();
         String location = mData.get(position).getLocation();
 
         DateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
         holder.beginning.setText(outputFormat.format(mData.get(position).getBeginningDate()));
-//        holder.ending.setText(outputFormat.format(mData.get(position).getEndingDate()));
+        holder.ending.setText(outputFormat.format(mData.get(position).getEndingDate()));
         holder.itineraryTitle.setText(title);
-        //holder.itineraryNumber.setText(String.format(context.getString(R.string.print_integer_number), itineraryNumber));
         holder.location.setText(location);
         holder.priceTagTextView.setText(String.format(context.getString(R.string.price_value), mData.get(position).getFullPrice()));
         Picasso.get().load(mData.get(position).getImageUrl()).into(holder.imageView);
@@ -106,7 +104,6 @@ public class ItineraryAdapter extends RecyclerView.Adapter<ItineraryAdapter.View
 
         //Defining variables of ITINERARY_LIST view
         TextView itineraryTitle;
-        TextView itineraryNumber;
         TextView location;
         TextView beginning;
         TextView ending;
@@ -117,7 +114,6 @@ public class ItineraryAdapter extends RecyclerView.Adapter<ItineraryAdapter.View
         ViewHolder(View itemView) {
             super(itemView);
             itineraryTitle = itemView.findViewById(R.id.itinerary_title);
-            //itineraryNumber = itemView.findViewById(R.id.itinerary_number);
             location = itemView.findViewById(R.id.location);
             beginning = itemView.findViewById(R.id.beginning);
             ending = itemView.findViewById(R.id.ending);
@@ -125,20 +121,6 @@ public class ItineraryAdapter extends RecyclerView.Adapter<ItineraryAdapter.View
             priceTagTextView = itemView.findViewById(R.id.itinerary_price_badge);
             itemView.setOnClickListener(this);
         }
-
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
     }
-
-
-    /**
-     * Item Click Listener for parent activity will implement this method to respond to click events.
-     */
-    interface ItemClickListener { //TODO: Remove?
-        void onItemClick(View view, int position);
-    }
-
 }
 
