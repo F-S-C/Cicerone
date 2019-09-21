a120dda3a46d193414e5a02f8553fefd3edcc6c0
espositoandrea <esposito_andrea99@hotmail.com>
2019-09-21 23:13:09 +0200

    Correzione di un errore di battitura

--- Server/database_connector/view/insert/InsertReservation.php
+++ Server/database_connector/view/insert/InsertReservation.php
@@ -8,12 +8,12 @@ require_once "/membri/fsc/database_connector/controller/insert/InsertReservation
 
 $connector = new InsertReservation();
 $connector->add_value(array(strtolower($_POST['username']),
-    ['booked_itinerary'], true)["itinerary_code"],
+    $_POST['booked_itinerary'],
     $_POST['number_of_children'],
     $_POST['number_of_adults'],
     $_POST['total'],
     $_POST['requested_date'],
     $_POST['forwarding_date'],
     $_POST['confirm_date']
-);
+));
 print $connector->get_content();
