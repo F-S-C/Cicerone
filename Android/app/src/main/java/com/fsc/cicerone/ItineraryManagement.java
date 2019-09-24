8d8e19ddd8ffbd15ba106f6b00b4d6cce41a7d12
espositoandrea <esposito_andrea99@hotmail.com>
2019-09-25 00:03:44 +0200

    Corretto errore

--- Android/app/src/main/java/com/fsc/cicerone/ItineraryManagement.java
+++ Android/app/src/main/java/com/fsc/cicerone/ItineraryManagement.java
@@ -22,6 +22,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
