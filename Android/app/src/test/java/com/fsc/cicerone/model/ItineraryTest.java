63caf2757ffb21384c57122ad8255a65b8cd6aa2
ReginaZaccaria <zaccariaregina@gmail.com>
2019-10-06 01:17:55 +0200

    Aggiornamento ItineraryTest (WIP)

--- Android/app/src/test/java/com/fsc/cicerone/model/ItineraryTest.java
+++ Android/app/src/test/java/com/fsc/cicerone/model/ItineraryTest.java
@@ -25,7 +25,9 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import java.lang.reflect.Field;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
@@ -92,118 +94,464 @@ public class ItineraryTest {
     }
 
     @Test
-    public void setCicerone() {
+    public void setCicerone() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+        final String string = "{\"username\":\"test\",\"tax_code\":\"IT0000000\",\"name\":\"test\",\"surname\":\"testsurname\",\"password\":\"$2y$10$nXW4njlKyYR4EmQoiQg3iee5xw9RfC7VeD\\/Z5t\\/UnX0xWAasnurq.\",\"email\":\"graziano.montanaro98@gmail.com\",\"user_type\":\"1\",\"cellphone\":\"0999561111\",\"birth_date\":\"1998-09-28\",\"sex\":\"male\",\"document\":{\"document_number\":\"test0000\",\"document_type\":\"Identity Card\",\"expiry_date\":\"2022-02-25\"},\"languages\":[]}";
+        final User cicerone = new User(string);
+
+        //when
+        itinerary.setCicerone(cicerone);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("cicerone");
+        field.setAccessible(true);
+        final User itineraryCicerone = (User) field.get(itinerary);
+        assertEquals("field wasn't retrieved properly", itineraryCicerone, cicerone);
     }
 
     @Test
-    public void setCode() {
+    public void setCode() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setCode(5);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("code");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 5);
     }
 
     @Test
-    public void getTitle() {
+    public void getTitle() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("title");
+        field.setAccessible(true);
+        field.set(itinerary, "title_test");
+
+        //when
+        final String result = itinerary.getTitle();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, "title_test");
     }
 
     @Test
-    public void setTitle() {
+    public void setTitle() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setTitle("title_test");
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("title");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), "title_test");
     }
 
     @Test
-    public void getDescription() {
+    public void getDescription() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        field.set(itinerary, "description_test");
+
+        //when
+        final String result = itinerary.getDescription();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, "description_test");
     }
 
     @Test
-    public void setDescription() {
+    public void setDescription() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setDescription("description_test");
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("description");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), "description_test");
     }
 
     @Test
-    public void getBeginningDate() {
+    public void getBeginningDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("beginningDate");
+        field.setAccessible(true);
+        field.set(itinerary, theDate);
+
+        //when
+        final Date result = itinerary.getBeginningDate();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, theDate);
     }
 
     @Test
-    public void setBeginningDate() {
+    public void setBeginningDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setBeginningDate(theDate);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("beginningDate");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(itinerary), theDate);
     }
 
     @Test
-    public void getEndingDate() {
+    public void getEndingDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("endingDate");
+        field.setAccessible(true);
+        field.set(itinerary, theDate);
+
+        //when
+        final Date result = itinerary.getEndingDate();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, theDate);
     }
 
     @Test
-    public void setEndingDate() {
+    public void setEndingDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setEndingDate(theDate);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("endingDate");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(itinerary), theDate);
     }
 
     @Test
-    public void getReservationDate() {
+    public void getReservationDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("reservationDate");
+        field.setAccessible(true);
+        field.set(itinerary, theDate);
+
+        //when
+        final Date result = itinerary.getReservationDate();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, theDate);
     }
 
     @Test
-    public void setReservationDate() {
+    public void setReservationDate() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Date theDate = new Date();
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setReservationDate(theDate);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("reservationDate");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(itinerary), theDate);
     }
 
     @Test
-    public void getLocation() {
+    public void getLocation() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("location");
+        field.setAccessible(true);
+        field.set(itinerary, "location_test");
+
+        //when
+        final String result = itinerary.getLocation();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, "location_test");
     }
 
     @Test
-    public void setLocation() {
+    public void setLocation() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setLocation("location_test");
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("location");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), "location_test");
     }
 
     @Test
-    public void getMinParticipants() {
+    public void getMinParticipants() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("minParticipants");
+        field.setAccessible(true);
+        field.set(itinerary, 1);
+
+        //when
+        final int result = itinerary.getMinParticipants();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 1);
     }
 
     @Test
-    public void setMinParticipants() {
+    public void setMinParticipants() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setMinParticipants(1);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("minParticipants");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 1);
     }
 
     @Test
-    public void getMaxParticipants() {
+    public void getMaxParticipants() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("maxParticipants");
+        field.setAccessible(true);
+        field.set(itinerary, 1);
+
+        //when
+        final int result = itinerary.getMaxParticipants();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 1);
     }
 
     @Test
-    public void setMaxParticipants() {
+    public void setMaxParticipants() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setMaxParticipants(10);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("maxParticipants");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 10);
     }
 
     @Test
-    public void getRepetitions() {
+    public void getRepetitions() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("repetitions");
+        field.setAccessible(true);
+        field.set(itinerary, 1);
+
+        //when
+        final int result = itinerary.getRepetitions();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 1);
     }
 
     @Test
-    public void setRepetitions() {
+    public void setRepetitions()  throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setRepetitions(1);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("repetitions");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 1);
     }
 
     @Test
-    public void getDuration() {
+    public void getDuration() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("duration");
+        field.setAccessible(true);
+        field.set(itinerary, "duration_test");
+
+        //when
+        final String result = itinerary.getDuration();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, "duration_test");
     }
 
     @Test
-    public void setDuration() {
+    public void setDuration()  throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setDuration("duration_test");
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("duration");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), "duration_test");
     }
 
     @Test
-    public void getFullPrice() {
+    public void getFullPrice() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("fullPrice");
+        field.setAccessible(true);
+        field.set(itinerary, 1.5f);
+
+        //when
+        final float result = itinerary.getFullPrice();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 1.5f,0);
     }
 
     @Test
-    public void setFullPrice() {
+    public void setFullPrice()  throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setFullPrice(1.5f);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("fullPrice");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 1.5f);
     }
 
     @Test
-    public void getReducedPrice() {
+    public void getReducedPrice() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("reducedPrice");
+        field.setAccessible(true);
+        field.set(itinerary, 1.5f);
+
+        //when
+        final float result = itinerary.getReducedPrice();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 1.5f,0);
     }
 
     @Test
-    public void setReducedPrice() {
+    public void setReducedPrice() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setReducedPrice(1.5f);
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("reducedPrice");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), 1.5f);
     }
 
     @Test
-    public void getImageUrl() {
+    public void getImageUrl() throws NoSuchFieldException, IllegalAccessException {
+        final Itinerary itinerary = new Itinerary();
+        final Field field = itinerary.getClass().getDeclaredField("imageUrl");
+        field.setAccessible(true);
+        field.set(itinerary, "https:\\/\\/fsc.altervista.org\\/images\\/1569059443-5d85f273504a7.jpg");
+
+        //when
+        final String result = itinerary.getImageUrl();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, "https:\\/\\/fsc.altervista.org\\/images\\/1569059443-5d85f273504a7.jpg");
     }
 
     @Test
-    public void setImageUrl() {
+    public void setImageUrl()  throws NoSuchFieldException, IllegalAccessException {
+        //given
+        final Itinerary itinerary = new Itinerary();
+
+        //when
+        itinerary.setImageUrl("https:\\/\\/fsc.altervista.org\\/images\\/1569059443-5d85f273504a7.jpg");
+
+        //then
+        final Field field = itinerary.getClass().getDeclaredField("imageUrl");
+        field.setAccessible(true);
+        assertEquals("field wasn't retrieved properly", field.get(itinerary), "https:\\/\\/fsc.altervista.org\\/images\\/1569059443-5d85f273504a7.jpg");
     }
 
     @Test
-    public void toJSONObject() {
+    public void toJSONObject() throws ParseException {
+        /*final String code = "1";
+        final String title = "title_test";
+        final String string = "{\"username\":\"test\",\"tax_code\":\"IT0000000\",\"name\":\"test\",\"surname\":\"testsurname\",\"password\":\"$2y$10$nXW4njlKyYR4EmQoiQg3iee5xw9RfC7VeD\\/Z5t\\/UnX0xWAasnurq.\",\"email\":\"graziano.montanaro98@gmail.com\",\"user_type\":\"1\",\"cellphone\":\"0999561111\",\"birth_date\":\"1998-09-28\",\"sex\":\"male\",\"document\":{\"document_number\":\"test0000\",\"document_type\":\"Identity Card\",\"expiry_date\":\"2022-02-25\"},\"languages\":[]}";
+        final User cicerone = new User(string);
+        final String description = "description_test";
+        final String theDate = "2019-03-31";
+        final String duration = "1";
+        final String location = "location_test";
+        final String minParticipants = "1";
+        final String maxParticipants = "2";
+        final String repetitions = "1";
+        final String full = "1.5";
+        final String reduced ="1.2";
+        final String image = "image_test";
+
+        JSONObject result = new JSONObject();
+        try {
+            result.put("itinerary_code", Integer.parseInt(code));
+            result.put("title", title);
+            result.put("username", cicerone);
+            result.put("description", description);
+            result.put("beginning_date", new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+            result.put("ending_date", new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+            result.put("end_reservations_date", new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+            result.put("duration", Integer.parseInt(duration));
+            result.put("location", location);
+            result.put("minimum_participants_number", Integer.parseInt(minParticipants));
+            result.put("maximum_participants_number",Integer.parseInt(maxParticipants));
+            result.put("repetitions_per_day", Integer.parseInt(repetitions));
+            result.put("full_price", Float.parseFloat(full));
+            result.put("reduced_price", Float.parseFloat(reduced));
+            result.put("image_url", image);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Itinerary itinerary = new Itinerary();
+        itinerary.setCode(Integer.parseInt(code));
+        itinerary.setTitle(title);
+        itinerary.setCicerone(cicerone);
+        itinerary.setDescription(description);
+        itinerary.setBeginningDate(new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+        itinerary.setEndingDate(new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+        itinerary.setReservationDate(new SimpleDateFormat("yyyy-MM-dd").parse(theDate));
+        itinerary.setDuration(duration);
+        itinerary.setLocation(location);
+        itinerary.setMinParticipants(Integer.parseInt(minParticipants));
+        itinerary.setMaxParticipants(Integer.parseInt(maxParticipants));
+        itinerary.setRepetitions(Integer.parseInt(repetitions));
+        itinerary.setFullPrice(Float.parseFloat(full));
+        itinerary.setReducedPrice(Float.parseFloat(reduced));
+        itinerary.setImageUrl(image);
+
+        assertEquals("Fields didn't match", itinerary.toJSONObject().toString(), result.toString());*/
     }
 }
